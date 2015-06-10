9002495a344bdce421ec1e137eae13e4558f3f5a
jackkav <jackkav@gmail.com>
2015-06-10 16:28:43 +0800

    first

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'pixl-xml',
+  version: '1.0.2',
+  summary: 'Wrapper for pixl-xml npm package',
+  git: 'https://github.com/jhuckaby/pixl-xml',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('pixl-xml.js');
+  api.export('XML');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('pixl-xml');
+  api.addFiles('pixl-xml-tests.js');
+});
---
+++ pixl-xml-tests.js
@@ -0,0 +1,59 @@
+// Write your tests here!
+// Here is an example.
+
+Tinytest.add('isDefined', function (test) {
+    var isDefined = false;
+    try {
+        XML;
+        isDefined = true;
+    }
+    catch (e) {
+    }
+    test.isTrue(isDefined, "XML is not defined");
+});
+
+Tinytest.add('canValidateBadXml', function (test) {
+  var xml = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
+  + '<QuestionFormAnswers xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionFormAnswers.xsd">\n'
+  + '  <Answer>\n'
+  + '    <QuestionIdentifier>Q2</QuestionIdentifier>\n'
+  + '    <FreeText>Change your thoughts and you change the world.</FreeText>\n'
+    // + '  </Answer>\n'
+    + '</QuestionFormAnswers>\n';
+    var doc = null;
+    try {
+        doc = XML.parse(xml);
+    }
+    catch (err) {
+        // console.log("XML Parser Error: " + err);
+        test.isTrue(true, "XML error is dealt with");
+        return;
+    }
+    test.isFalse(true, "XML error was not dealt with");
+});
+
+Tinytest.add('canParseXml', function (test) {
+  var xml = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
+  + '<QuestionFormAnswers xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionFormAnswers.xsd">\n'
+  + '  <Answer>\n'
+  + '    <QuestionIdentifier>Q2</QuestionIdentifier>\n'
+  + '    <FreeText>Change your thoughts and you change the world.</FreeText>\n'
+  + '  </Answer>\n'
+  + '</QuestionFormAnswers>\n';
+  var xmlDoc = XML.parse(xml);
+  test.isTrue(xmlDoc, "XML is parsed");
+});
+
+Tinytest.add('canParseXmlandGetValue', function (test) {
+  var xml = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
+  + '<QuestionFormAnswers xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionFormAnswers.xsd">\n'
+  + '  <Answer>\n'
+  + '    <QuestionIdentifier>Q2</QuestionIdentifier>\n'
+  + '    <FreeText>Change your thoughts and you change the world.</FreeText>\n'
+  + '  </Answer>\n'
+  + '</QuestionFormAnswers>\n';
+  var xmlDoc = XML.parse(xml);
+  var q = xmlDoc.Answer.QuestionIdentifier;
+  var result = q ==="Q2";
+  test.isTrue(q, "XML is parsed");
+});
